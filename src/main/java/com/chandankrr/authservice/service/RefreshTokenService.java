0998b09c93da4f5d001f44fe11a21df182f2990c
Chandan Kumar <chandankrr.91@gmail.com>
2024-05-03 21:34:02 +0530

    Implement logic for authentication

---
+++ src/main/java/com/chandankrr/authservice/service/RefreshTokenService.java
@@ -0,0 +1,43 @@
+package com.chandankrr.authservice.service;
+
+import com.chandankrr.authservice.entity.RefreshToken;
+import com.chandankrr.authservice.entity.UserInfo;
+import com.chandankrr.authservice.repository.RefreshTokenRepository;
+import com.chandankrr.authservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class RefreshTokenService {
+
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final UserRepository userRepository;
+
+    public RefreshToken createRefreshToken(String username) {
+        UserInfo userInfoExtracted = userRepository.findByUsername(username);
+        RefreshToken refreshToken = RefreshToken.builder()
+                .userInfo(userInfoExtracted)
+                .token(UUID.randomUUID().toString())
+                .expiryDate(Instant.now().plusMillis(600000))
+                .build();
+        return refreshTokenRepository.save(refreshToken);
+    }
+
+    public Optional<RefreshToken> findByToken(String token) {
+        return refreshTokenRepository.findByToken(token);
+    }
+
+    public RefreshToken verifyExpiration(RefreshToken token) {
+        if (token.getExpiryDate().compareTo(Instant.now()) < 0) {
+            refreshTokenRepository.delete(token);
+            throw new RuntimeException(token.getToken() + " Refresh token is expired. Please make a new login..!");
+        }
+        return token;
+    }
+
+}
---
+++ src/main/java/com/chandankrr/authservice/service/UserDetailsServiceImpl.java
@@ -0,0 +1,53 @@
+package com.chandankrr.authservice.service;
+
+import com.chandankrr.authservice.entity.UserInfo;
+import com.chandankrr.authservice.model.UserInfoDto;
+import com.chandankrr.authservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserInfo user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("could not found user..!!");
+        }
+        return new CustomUserDetails(user);
+    }
+
+    public UserInfo checkIfUserAlreadyExist(UserInfoDto userInfoDto) {
+        return userRepository.findByUsername(userInfoDto.getUsername());
+    }
+
+    public Boolean signupUser(UserInfoDto userInfoDto) {
+        //        ValidationUtil.validateUserAttributes(userInfoDto);
+        userInfoDto.setPassword(passwordEncoder.encode(userInfoDto.getPassword()));
+        if (Objects.nonNull(checkIfUserAlreadyExist(userInfoDto))) {
+            return false;
+        }
+        String userId = UUID.randomUUID().toString();
+        UserInfo user = UserInfo.builder()
+                .userId(userId)
+                .username(userInfoDto.getUsername())
+                .password(userInfoDto.getPassword())
+                .roles(new HashSet<>())
+                .build();
+        userRepository.save(user);
+        return true;
+    }
+}
